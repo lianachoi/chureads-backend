f4fd359fe106f678e551e20b7ca04d1353c3384f
lianachoi <2ndsunmail@gmail.com>
2025-07-18 14:17:58 +0900

    운영 할 땐 production모드로사용하도록

--- database/db.js
+++ database/db.js
@@ -8,7 +8,8 @@ export const connectDB = async () => {
         if (db) {
             return db;
         }
-        const MONGODB_URI = process.env.MONGODB_URI_LOCAL;
+        const MONGODB_URI = process.env.NODE_ENV === "development"? 
+        process.env.MONGODB_URI_LOCAL : process.env.MONGODB_URI_ATLAS;
         const DB_NAME = process.env.DB_NAME;
 
         const client = new MongoClient(MONGODB_URI);
